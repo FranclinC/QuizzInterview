d146272a7a2598548a4d76784e8045c300d9dd87
Franclin Cabral <franclin.oliveira@invillia.com>
2020-11-01 22:28:59 -0300

    Adding basic functionalities, timer, score

--- QuizzInterview/Quizz/View/QuizzViewController.swift
+++ QuizzInterview/Quizz/View/QuizzViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 class QuizzViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textField: UITextField! {
+        didSet {
+            textField.placeholder = "Insert word"
+            
+        }
+    }
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var pointsLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
@@ -33,26 +38,86 @@ class QuizzViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.viewDelegate = self
+        setupTableView()
+        setupTextField()
+        setupButton()
         viewModel.fetch()
         
     }
     
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.register(cellType: CorrectAnswerTableViewCell.self)
+        tableView.estimatedRowHeight = 200
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    private func setupButton() {
+        actionButton.setTitle("Start", for: .normal)
+        actionButton.isEnabled = false
+    }
+    
+    private func setupTextField() {
+        textField.isEnabled = false
+        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
     @IBAction func didTapActionButton(_ sender: UIButton) {
-        
+        textField.isEnabled = true
+        textField.becomeFirstResponder()
+        viewModel.startCountDown()
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        viewModel.addToCorrectIfNeeded(textField.text)
     }
     
 }
 
 extension QuizzViewController: QuizzViewModelDelegate {
     func ready(_ viewModel: QuizzViewModel) {
-        print("Ready for use")
+        DispatchQueue.main.async {
+            self.titleLabel.text = viewModel.getTitle()
+            self.pointsLabel.text = self.viewModel.getScore()
+            self.actionButton.isEnabled = true
+        }
+        
+        
+        //Start Timer
     }
     
     func showError(_ viewModel: QuizzViewModel, error: QuizzError) {
         print("Should show error")
     }
     
+    func correctAnswer(_ viewModel: QuizzViewModel) {
+        DispatchQueue.main.async {
+            self.textField.text?.removeAll()
+            self.tableView.reloadData()
+            self.pointsLabel.text = self.viewModel.getScore()
+        }
+        
+    }
     
-    
-    
+    func update(_ viewModel: QuizzViewModel, time: String) {
+        DispatchQueue.main.async {
+            self.timeLabel.text = time
+        }
+    }
+   
 }
+
+extension QuizzViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows(section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(class: CorrectAnswerTableViewCell.self, indexPath: indexPath) { (cell) in
+            cell.setup(self.viewModel.getAnswer(for: indexPath.row))
+        }
+    }
+}
+
+
+
--- QuizzInterview/Quizz/ViewModel/QuizzViewModel.swift
+++ QuizzInterview/Quizz/ViewModel/QuizzViewModel.swift
@@ -6,9 +6,13 @@
 //  Copyright © 2020 Franclin Cabral Menezes de Oliveira. All rights reserved.
 //
 
+import Foundation
+
 protocol QuizzViewModelDelegate: AnyObject {
     func ready(_ viewModel: QuizzViewModel)
     func showError(_ viewModel: QuizzViewModel, error: QuizzError)
+    func correctAnswer(_ viewModel: QuizzViewModel)
+    func update(_ viewModel: QuizzViewModel, time: String)
     
 }
 
@@ -19,6 +23,10 @@ class QuizzViewModel{
     
     private var quizz: Quizz?
     private var correctAnswered: [String]
+    
+    var timer: Timer?
+    private var defaultTime: Int = 300
+    private var currentTime: Int = 300
     
     init(service: QuizzService) {
         self.service = service
@@ -33,6 +41,7 @@ class QuizzViewModel{
             case .success(let quizz):
                 self.quizz = quizz
                 self.viewDelegate?.ready(self)
+                self.getCurrentStringTime()
             case .failure(let error):
                 self.viewDelegate?.showError(self, error: error)
             }
@@ -40,10 +49,61 @@ class QuizzViewModel{
         
     }
     
+    func numberOfRows(_ section: Int) -> Int {
+        return correctAnswered.count
+    }
+    
+    func getAnswer(for index: Int) -> String? {
+        guard correctAnswered.indices.contains(index) else { return nil }
+        return correctAnswered[index]
+    }
+    
+    func getTitle() -> String {
+        guard let title = quizz?.question else { return "" }
+        return title
+    }
+    
+    func getScore() -> String {
+        guard let answers = quizz?.answer else { return "--/--" }
+        return "\(correctAnswered.count)/\(answers.count)"
+    }
+    
+    func addToCorrectIfNeeded(_ text: String?) {
+        guard let text = text else { return }
+        if isACorrectOption(text) && !isAlreadyInUse(text) {
+            correctAnswered.append(text)
+            viewDelegate?.correctAnswer(self)
+        }
+    }
+    
+    func startCountDown() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + DispatchTimeInterval.seconds(defaultTime)) {
+            self.timer?.invalidate()
+        }
+    }
+    
+    private func getCurrentStringTime() {
+        let minutes = currentTime / 60
+        let seconds = currentTime % 60
+        
+        let text = String(format: "%02d:%02d", minutes, seconds)
+        viewDelegate?.update(self, time: text)
+    }
+    
+    @objc private func countDown() {
+        currentTime -= 1
+        getCurrentStringTime()
+    }
+    
+    private func isAlreadyInUse(_ text: String) -> Bool {
+        return correctAnswered.contains(text)
+    }
+    
     private func isACorrectOption(_ text: String) -> Bool {
         guard let quizz = quizz else { return false }
         return quizz.answer.contains(text)
-        
     }
     
 }
